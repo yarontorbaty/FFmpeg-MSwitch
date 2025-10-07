fb7a674c845d2128d9f7c9928d34717337eed619
Yaron Torbaty <yarontorbaty@gmail.com>
2025-10-06 19:34:13 -0700

    Add Windows compatibility to mswitchdirect demuxer
    - Add winsock2.h headers for Windows networking
    - Keep mswitch (old experimental) disabled on Windows
    - mswitchdirect (main demuxer) now works on all platforms
    - Define close as closesocket on Windows

--- libavformat/mswitchdirect.c
+++ libavformat/mswitchdirect.c
@@ -30,12 +30,20 @@
  */
 
 #include <pthread.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <io.h>
+#define close closesocket
+#else
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <fcntl.h>
-#include <errno.h>
 #include <unistd.h>
+#endif
 
 #include "libavutil/avstring.h"
 #include "libavutil/error.h"
